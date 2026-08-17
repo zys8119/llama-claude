9e41d5225aa91360021d2d863910ad90dfa751c7
zys8119 <770959294@qq.com>
2026-08-17 19:45:40 +0800

    init

--- tools.ts
+++ tools.ts
@@ -2,13 +2,6 @@ import OpenAI from "openai";
 import getWeather from "./tools/get_weather.ts";
 import getCurrentTime from "./tools/current_time.ts";
 import writeFile from "./tools/write_file.ts";
-import { sync } from "glob";
-import fsExtra from "fs-extra";
-console.log(
-  sync(["./tools/**"], { absolute: true }).map((file) =>
-    fsExtra.readFileSync(file),
-  ),
-);
 export const toolsRegister = [
   getWeather,
   getCurrentTime,
