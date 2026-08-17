ecfb649d5ff6027e8cb7f22a8ad893b9a26e654c
zys8119 <770959294@qq.com>
2026-08-17 22:56:55 +0800

    init

---
+++ App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class='App'></div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped lang="less">
+.App {}
+</style>
---
+++ a.ts
@@ -0,0 +1,4 @@
+import { createTerminal } from "@simon_he/vue-tui";
+import App from "./App.vue";
+
+createTerminal(App).mount();
