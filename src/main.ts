d7a77fc4a7440556cf45184e77945289af7a33f2
zys8119 <770959294@qq.com>
2026-08-20 09:35:57 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
-import run from "./tui/cli.tsx";
-run();
+// import run from "./tui/cli.tsx";
+// run();
+import { input } from "@inquirer/prompts";
+import { onSubmit } from "./openai.ts";
+(async function run() {
+  const question = await input({
+    message: "请输入您的问题",
+  });
+  await onSubmit(question);
+  await run();
+})().catch(console.error);
--- src/openai.ts
+++ src/openai.ts
@@ -1,7 +1,7 @@
 import { OpenAI } from "openai";
 import { search } from "@inquirer/prompts";
 import chalk from "chalk";
-import { ref } from "./effect.ts";
+import { ref, computed, effect } from "./effect.ts";
 const openai = new OpenAI({
   apiKey: "sk-",
   baseURL: "http://127.0.0.1:8080/v1",
@@ -11,30 +11,54 @@ export function stdinResume() {
   process.stdin.resume();
 }
 export const models = (await openai.models.list()).data.map((e) => e.id);
-export const model = await search({
-  message: "请选择模型",
-  source: (input) => (input ? models.filter((e) => e.includes(input)) : models),
-});
+export const model =
+  "Qwen3-0.6B-Q8_0" ||
+  (await search({
+    message: "请选择模型",
+    source: (input) =>
+      input ? models.filter((e) => e.includes(input)) : models,
+  }));
 stdinResume();
 console.log(chalk.green(`✅ 已选择模型: ${model}`));
 export default openai;
 export const chatMessageLists = ref([]);
-export const chat = async ({
-  text,
-  controller,
-}: {
-  text: string;
-  controller: AbortController;
-}) => {
+export const chatMessagesData = computed(() =>
+  chatMessageLists.value.map((e: any) => {
+    switch (e.type) {
+      case "user":
+        return {
+          role: "user",
+          content: e.content,
+        };
+      case "assistant":
+        return {
+          role: "assistant",
+          content: e.content,
+        };
+      default:
+        return e;
+    }
+  }),
+);
+
+export function pullSystemPrompt() {
+  return [
+    {
+      role: "system",
+      content: "你是一个专业的助手，你的任务是回答用户的问题。",
+    },
+  ];
+}
+type ChatMessages = Parameters<
+  typeof openai.chat.completions.create
+>[0]["messages"];
+export const chat = async ({ controller }: { controller: AbortController }) => {
+  console.log(chalk.blue(chatMessagesData.value.at(-1).content));
+  const systemPrompt = await pullSystemPrompt();
   const response = await openai.chat.completions.create(
     {
       model: model,
-      messages: [
-        {
-          role: "user",
-          content: text,
-        },
-      ],
+      messages: systemPrompt.concat(chatMessagesData.value) as ChatMessages,
       stream: true,
     },
     {
@@ -43,7 +67,7 @@ export const chat = async ({
   );
   for await (const chunk of response) {
     let assistantMessage = chatMessageLists.value.find(
-      (e) => e.id && e.id === chunk.id,
+      (e: any) => e.id && e.id === chunk.id,
     );
     if (!assistantMessage) {
       assistantMessage = chunk;
@@ -58,11 +82,14 @@ export const chat = async ({
       reasoning_content?: string;
     };
     if (delta.reasoning_content) {
+      process.stdout.write(chalk.gray(delta?.reasoning_content || ""));
       assistantMessage.reasoning_content += delta?.reasoning_content || "";
     } else {
+      process.stdout.write(chalk.green(delta?.content || ""));
       assistantMessage.content += delta?.content || "";
     }
   }
+  process.stdout.write("\n");
 };
 export const controllerCache = [] as AbortController[];
 export const abortAllChat = () => {
@@ -70,15 +97,14 @@ export const abortAllChat = () => {
   controllerCache.length = 0;
 };
 
-export const onSubmit = (text: string) => {
+export const onSubmit = async (text: string) => {
   chatMessageLists.value.push({
     role: "user",
     content: text,
   });
   const controller = new AbortController();
   controllerCache.push(controller);
-  chat({
-    text,
+  await chat({
     controller,
   })
     .catch((err) => {
@@ -87,10 +113,4 @@ export const onSubmit = (text: string) => {
     .finally(() => {
       controllerCache.splice(controllerCache.indexOf(controller), 1);
     });
-
-  return {
-    abort: () => {
-      controller.abort();
-    },
-  };
 };
